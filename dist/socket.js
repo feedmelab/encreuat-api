fb93e79d23ca9dabc490051f1734e0349c7edb13
feedmelab <xtorner@gmail.com>
2022-05-05 19:07:10 +0200

    retoques

--- dist/socket.js
+++ dist/socket.js
@@ -2,12 +2,12 @@
 exports.__esModule = true;
 var socket_controllers_1 = require("socket-controllers");
 var socket_io_1 = require("socket.io");
-exports["default"] = (function (httpServer) {
-    var io = new socket_io_1.Server(httpServer, {
-        cors: {
-            origin: "*"
-        }
-    });
-    socket_controllers_1.useSocketServer(io, { controllers: [__dirname + "/api/controllers/*.ts"] });
-    return io;
-});
+exports["default"] = function (httpServer) {
+	var io = new socket_io_1.Server(httpServer, {
+		cors: {
+			origin: "https://jocs.feedmelab.com:8080",
+		},
+	});
+	socket_controllers_1.useSocketServer(io, { controllers: [__dirname + "/api/controllers/*.js"] });
+	return io;
+};

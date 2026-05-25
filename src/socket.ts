c67d00150f0b0a585cc9b405c995c138bf0956ef
Xavi Torner Molins <xtorner@taxitronic.com>
2026-05-25 16:49:47 +0200

    http

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,3 @@
-import { useSocketServer } from "socket-controllers";
 import { Server } from "socket.io";
 import { GameController } from "./api/controllers/gameController";
 import { MainController } from "./api/controllers/mainController";
@@ -10,6 +9,42 @@ export default (httpServer) => {
 			origin: "*",
 		},
 	});
-	useSocketServer(io, { controllers: [GameController, MainController, RoomController] });
+
+	const roomController = new RoomController();
+	const gameController = new GameController();
+	const mainController = new MainController();
+
+	io.on("connection", (socket) => {
+		mainController.onConnection(socket, io);
+
+		socket.on("get_open_games", () => {
+			roomController.getOpenGamesList(io, socket);
+		});
+
+		socket.on("create_game", async () => {
+			await roomController.createGame(io, socket);
+		});
+
+		socket.on("join_game", async (message) => {
+			await roomController.joinGame(io, socket, message);
+		});
+
+		socket.on("cancel_game", async (message) => {
+			await roomController.cancelGame(io, socket, message);
+		});
+
+		socket.on("update_game", async (message) => {
+			await gameController.updateGame(io, socket, message);
+		});
+
+		socket.on("report_match_winner", (message) => {
+			gameController.reportMatchWinner(io, message);
+		});
+
+		socket.on("get_winners_board", () => {
+			gameController.getWinnersBoard(io, socket);
+		});
+	});
+
 	return io;
 };
